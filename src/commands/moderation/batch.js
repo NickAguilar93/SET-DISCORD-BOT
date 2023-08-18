d30b8d3795c0e49bd9392627f68476e8f63d089c
NickAguilar93 <99554672+NickAguilar93@users.noreply.github.com>
2023-08-18 13:44:19 -0400

    can now fetch all members of a role into a collection and add a role to exist role

--- src/commands/moderation/batch.js
+++ src/commands/moderation/batch.js
@@ -1,4 +1,15 @@
+require("dotenv").config();
+const { Client, Collection, Events, IntentsBitField } = require("discord.js");
 const { SlashCommandBuilder } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,6 +26,18 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    await interaction.reply("Finished!");
+    const target = interaction.options.getRole("role1");
+    const selection = interaction.options.getRole("role2");
+
+    client.on("messageCreate", async (msg) => {
+      let memberList = msg.guild.roles.cache.get(target.id).members.map(m => m.user.id);
+      for(let member in memberList) {
+      const memberid = await msg.guild.members.fetch(memberList[member]);
+      // memberid.roles.remove(target.id);
+      memberid.roles.add(selection.id);
+      }
+    });
+    await interaction.reply("Done!");
   },
 };
+client.login(process.env.TOKEN);
