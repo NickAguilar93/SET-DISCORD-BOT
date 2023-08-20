37c971f243af4ed7cfd6b68c88f4134e8ac46764
NickAguilar93 <99554672+NickAguilar93@users.noreply.github.com>
2023-08-20 12:14:56 -0400

    removed comments

--- src/commands/moderation/batch.js
+++ src/commands/moderation/batch.js
@@ -35,31 +35,6 @@ module.exports = {
         .get(target.id)
         .members.map((m) => m.user.id);
       for (let member in memberList) {
-        // const memberid = await msg.guild.members.fetch(memberList[member]);
-        // //Make sure bot role has permission
-        // console.log(`\nWorking on User ${memberid.displayName}`);
-        // await memberid.roles.remove(target.id).then(memberid.roles.add(selection.id));
-        // const user = await msg.guild.members.fetch({user:memberid, force:true});
-        // console.log(user);
-        // if (user.role) {
-        //   console.log(
-        //     `Role ${selection.name} was successfully added to user ${user.displayName}`
-        //   );
-        // } else {
-        //   console.log(
-        //     `Role ${selection.name} was not added to user ${user.displayName}`
-        //   );
-        // }
-
-        // if (!user.roles.cache.has(target.id)) {
-        //   console.log(
-        //     `Role ${target.name} was successfully removed from user ${user.displayName}`
-        //   );
-        // } else {
-        //   console.log(
-        //     `Role ${target.name} was not removed from user ${user.displayName}`
-        //   );
-        // }
         let currentUser = await msg.guild.members.fetch(memberList[member]);
         if (currentUser.roles.cache.has(target.id)) {
           await currentUser.roles.remove(target.id);
