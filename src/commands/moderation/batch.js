54d1a8d9a4bfc2b82cce22b62938b9801927a073
NickAguilar93 <99554672+NickAguilar93@users.noreply.github.com>
2023-08-20 16:09:28 -0400

    some logging for errors added

--- src/commands/moderation/batch.js
+++ src/commands/moderation/batch.js
@@ -33,9 +33,18 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
 
   async execute(interaction) {
+    const clientID = interaction.guild.id;
     const targetRole = interaction.options.getRole("role1");
     const desiredRole = interaction.options.getRole("role2");
-    let list = client.guilds.cache.get(process.env.GUILD_ID);
+    let errorCounter = 0;
+    let errorList = [
+      {
+        username: "",
+        description: "",
+      },
+    ];
+    
+    let list = client.guilds.cache.get(clientID);
 
     try {
       await list.members.fetch();
@@ -47,12 +56,33 @@ module.exports = {
       membersInRole.forEach(async (element) => {
         await element.roles.remove(targetRole.id);
         await element.roles.add(desiredRole.id);
+
+        if (element.roles.cache.has(targetRole.id)) {
+          errorList.push({
+            name: element.displayName,
+            description: "role was not removed",
+          });
+          errorCounter++;
+        } else if (!element.roles.cache.has(desiredRole.id)) {
+          errorList.push({
+            name: element.displayName,
+            description: "role was not added",
+          });
+          errorCounter++;
+        }
       });
       // console.log(role1);
     } catch (err) {
       console.error(err);
     }
-    await interaction.reply("Done!");
+
+    if (errorCounter > 0) {
+      errorList.forEach(element => {
+        console.log(`User: ${element.name} ${description}`);
+      })
+    }
+
+    await interaction.reply(`${errorCounter} errors occured`);
   },
 };
 client.login(process.env.TOKEN);
